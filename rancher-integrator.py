fc85758d4b77f170ab8b3a024571fc18d622d054
Bayani Carbone <bayani.carbone@canonical.com>
2021-07-09 21:02:43 +0000

    renamed delete to unregister

--- rancher-integrator.py
+++ rancher-integrator.py
@@ -2,6 +2,8 @@ import rancher
 import petname
 import argparse
 import time
+import urllib
+import ssl
 
 class RancherRegsitration:
     def __init__(self, rancher_url, access_key, secret_key, cert_verify=True):
@@ -19,13 +21,17 @@ class RancherRegsitration:
         # import cluster
         cluster_data = self.client.create_cluster(name=name)
 
+        time.sleep(5)
         # create registration token for the cluster
         reg_data = self.client.create_clusterRegistrationToken(clusterId=cluster_data.data_dict()["id"])
 
+        linkToFile = reg_data.data_dict()['manifestUrl']
+        localDestination = "./import_manifest/import_" + name + ".yaml"
+        resultFilePath, responseHeaders = urllib.request.urlretrieve(linkToFile, localDestination)
         # get url of k8s import manifest
         return name + ' - ' + reg_data.data_dict()['manifestUrl']
 
-    def delete_cluster(self, name):
+    def unregister_cluster(self, name):
         # get cluster by name
         cluster = self.client.list_cluster(name=name)
 
@@ -33,7 +39,7 @@ class RancherRegsitration:
         cluster_id = cluster.data[0]['id']
         cluster = self.client.by_id_cluster(cluster_id)
 
-        # delete cluster
+        # unregister cluster
         resp = self.client.delete(cluster)
 
         return resp
@@ -53,16 +59,16 @@ def main():
     parser.add_argument('-i', '--insecure', help='Allow insecure https', action="store_true")
     parser.add_argument('-d', '--daemon', help='Run forever', action="store_true")
 
-    # subparser for the different commands e.g. register, delete
+    # subparser for the different commands e.g. register, unregister
     subparsers = parser.add_subparsers(dest='command', help='Sub-commands')
 
     # register subparser
     register_parser = subparsers.add_parser('register', help='REGISTER a cluster in rancher')
     register_parser.add_argument('-n', '--name', help='new cluster name')
 
-    # delete subparser
-    delete_parser = subparsers.add_parser('delete', help='DELETE a cluster from rancher')
-    delete_parser.add_argument('name', help='cluster name')
+    # unregister subparser
+    unregister_parser = subparsers.add_parser('unregister', help='unregister a cluster from rancher')
+    unregister_parser.add_argument('name', help='cluster name')
 
     args = parser.parse_args()
     if args.command is not None:
@@ -72,6 +78,7 @@ def main():
 
         # create RancherRegsitration object with connection details
         if args.insecure:
+            ssl._create_default_https_context = ssl._create_unverified_context
             r = RancherRegsitration(rancher_url, access_key, secret_key, False)
         else:
             r = RancherRegsitration(rancher_url, access_key, secret_key)
@@ -79,8 +86,8 @@ def main():
         if args.command == 'register':
             manifest = r.register_cluster(args.name)
             print(manifest)
-        elif args.command == 'delete':
-            resp = r.delete_cluster(args.name)
+        elif args.command == 'unregister':
+            resp = r.unregister_cluster(args.name)
             print(resp)
 
         if args.daemon:
