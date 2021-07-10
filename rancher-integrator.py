ff2958c7355fd974b2195b79f0129d8b5d47ba30
Bayani Carbone <bayani.carbone@canonical.com>
2021-07-10 23:22:16 +0000

    added exception handling and refactored retrieval of import manifest

--- rancher-integrator.py
+++ rancher-integrator.py
@@ -4,43 +4,126 @@ import argparse
 import time
 import urllib
 import ssl
+import sys
+import requests
+import logging
+import re
+import os
+from pathlib import Path
 
 class RancherRegsitration:
     def __init__(self, rancher_url, access_key, secret_key, cert_verify=True):
         self.client = self._create_client(rancher_url, access_key, secret_key, cert_verify)
 
     def _create_client(self, rancher_url, access_key, secret_key, cert_verify):
-        return rancher.Client(url=rancher_url,
+        '''Create a client for the Rancher API'''
+
+        try:
+            client = rancher.Client(url=rancher_url,
                                 access_key=access_key,
                                 secret_key=secret_key,
                                 verify=cert_verify)
+        except requests.exceptions.ConnectionError as err:
+            if 'Temporary failure in name resolution' in str(err):
+                logging.error('Failed to create a Rancher API client, please check the Rancher API url %s.', rancher_url)
+                sys.exit()
+            elif 'CERTIFICATE_VERIFY_FAILED' in str(err):
+                logging.error('Failed to create a Rancher API client due to certificate verification failure. Perhaps you might want to try with the --insecure option.')
+                sys.exit()
+        except rancher.ApiError as err:
+            logging.error('Failed to create a Rancher API client, please check the API credentials: %s.', err)
+            sys.exit()
+
+        return client
 
     def register_cluster(self, name):
+        '''Register a cluster in a Rancher platform and retrieve the Kubernetes import manifest'''
+
         if name is None:
             name = petname.Generate()
+
         # import cluster
-        cluster_data = self.client.create_cluster(name=name)
+        try:
+            cluster_data = self.client.create_cluster(name=name)
+        except rancher.ApiError as err:
+            if re.search(r'Forbidden.*cannot create resource "clusters"', str(err)):
+                logging.error('Failed to register cluster %s, please check the API credentials as they do not allow the creation of clusters.', name)
+                sys.exit()
+            elif re.search(r'NotUnique.*Cluster name', str(err)):
+                logging.error('Failed to register cluster %s, a cluster with the same name already exists.', name)
+                sys.exit()
+            elif 'InvalidFormat' in str(err):
+                logging.error('Failed to register cluster, the format of the cluster name is invalid: %s.', name)
+                sys.exit()
+            else:
+                logging.error('Failed to register cluster %s, API error: %s.', name, err)
+                sys.exit()
 
-        time.sleep(5)
         # create registration token for the cluster
-        reg_data = self.client.create_clusterRegistrationToken(clusterId=cluster_data.data_dict()["id"])
+        token_created = False
+        count = 0
+        while count < 5 and not token_created:
+            try:
+                reg_data = self.client.create_clusterRegistrationToken(clusterId=cluster_data.data_dict()['id'])
+                token_created = True
+            except rancher.ApiError as err:
+                if re.search(r'Forbidden.*cannot create resource "clusterregistrationtokens"', str(err)):
+                    logging.warning('Unable to retrieve the cluster registration token for cluster: %s. Will retry in 1 second.', name)
+                    count += 1
+                    time.sleep(1)
+        if count == 5:
+            logging.error('Failed to retrieve a cluster registration token for cluster: %s. Max retry attempts reached.', name)
+            sys.exit()
 
-        linkToFile = reg_data.data_dict()['manifestUrl']
-        localDestination = "./import_manifest/import_" + name + ".yaml"
-        resultFilePath, responseHeaders = urllib.request.urlretrieve(linkToFile, localDestination)
-        # get url of k8s import manifest
-        return name + ' - ' + reg_data.data_dict()['manifestUrl']
+        # fetch registration token for the cluster
+        import_manifest_url = reg_data.data_dict()['manifestUrl']
+        storage_directory = Path('import_manifest')
+
+        # if the destination directory already exists, just delete it
+        if storage_directory.exists():
+            for f in storage_directory.iterdir():
+                f.unlink()
+            storage_directory.rmdir()
+        storage_directory.mkdir()
+        manifest_file_name = name + '.yaml'
+
+        try:
+            resultFilePath, responseHeaders = urllib.request.urlretrieve(import_manifest_url, storage_directory / manifest_file_name)
+        except urllib.error.HTTPError as err:
+            logging.error('Failed to retrieve the import manifest (%s) for cluster: %s. Error: %s', import_manifest_url, name, err)
+            self.unregister_cluster(name)
+            sys.exit()
+
+        # return name, url and location of path of import manifest
+        return [name, import_manifest_url, str(storage_directory / manifest_file_name)]
 
     def unregister_cluster(self, name):
         # get cluster by name
-        cluster = self.client.list_cluster(name=name)
+        try:
+            cluster = self.client.list_cluster(name=name)
+        except rancher.ApiError as err:
+            logging.error('Failed to unregister cluster %s, API error', err)
+            sys.exit()
 
         # get cluster by id
-        cluster_id = cluster.data[0]['id']
-        cluster = self.client.by_id_cluster(cluster_id)
+        try:
+            cluster_id = cluster.data[0]['id']
+        except IndexError:
+            logging.error('Failed to unregister cluster, %s is not a registered cluster.', name)
+            sys.exit()
+
+        try:
+            cluster = self.client.by_id_cluster(cluster_id)
+        except rancher.ApiError as err:
+            logging.error('Failed to unregister cluster %s, API error', err)
+            sys.exit()
 
         # unregister cluster
-        resp = self.client.delete(cluster)
+        try:
+            resp = self.client.delete(cluster)
+        except rancher.ApiError as err:
+            logging.error('Failed to unregister cluster %s, API error', err)
+            sys.exit()
 
         return resp
 
@@ -56,8 +139,8 @@ def main():
     parser.add_argument('url', help='Rancher url')
     parser.add_argument('username', help='API access key')
     parser.add_argument('password', help='API secret key')
-    parser.add_argument('-i', '--insecure', help='Allow insecure https', action="store_true")
-    parser.add_argument('-w', '--wait', help='Run forever', action="store_true")
+    parser.add_argument('-i', '--insecure', help='Allow insecure https', action='store_true')
+    parser.add_argument('-w', '--wait', help='Run forever', action='store_true')
 
     # subparser for the different commands e.g. register, unregister
     subparsers = parser.add_subparsers(dest='command', help='Sub-commands')
@@ -101,5 +184,5 @@ def main():
     else:
         parser.print_help()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
