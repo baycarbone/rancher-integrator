4588857bf8079d686cd8bddfdd0157de711e7b66
Bayani Carbone <bayani.carbone@canonical.com>
2021-07-12 01:17:49 +0000

    made correction to be able to use environment variables, fixed exception handling for other ConnectionError when creating a client and added the ability to wait instead of exit in case of error

--- rancher-integrator.py
+++ rancher-integrator.py
@@ -14,12 +14,13 @@ import random
 from json.decoder import JSONDecodeError
 
 class RancherRegsitration:
-    def __init__(self, rancher_url, access_key, secret_key, cert_verify=True):
+    def __init__(self, rancher_url, access_key, secret_key, wait='False', cert_verify=True):
         # clear log file
         with open('error.log', 'w'):
             pass
         logging.basicConfig(filename='error.log', level=logging.ERROR)
         logging.getLogger().addHandler(logging.StreamHandler())
+        self.wait = wait
         self.client = self._create_client(rancher_url, access_key, secret_key, cert_verify)
 
     def _create_client(self, rancher_url, access_key, secret_key, cert_verify=True):
@@ -33,16 +34,19 @@ class RancherRegsitration:
         except requests.exceptions.ConnectionError as err:
             if 'Temporary failure in name resolution' in str(err):
                 logging.error('Failed to create a Rancher API client, please check the Rancher API url %s.', rancher_url)
-                sys.exit()
+                self.exit_or_wait()
             elif 'CERTIFICATE_VERIFY_FAILED' in str(err):
-                logging.error('Failed to create a Rancher API client due to certificate verification failure. Perhaps you might want to try with the --insecure option.')
-                sys.exit()
+                logging.error('Failed to create a Rancher API client due to certificate verification failure. Perhaps you might want to try with the --cert_check option set to False.')
+                self.exit_or_wait()
+            else:
+                logging.error('Failed to create a Rancher API client, please check the API connection details. ConnectionError: %s', err)
+                self.exit_or_wait()
         except rancher.ApiError as err:
-            logging.error('Failed to create a Rancher API client, please check the API credentials: %s.', err)
-            sys.exit()
+            logging.error('Failed to create a Rancher API client, please check the API credentials. ApiError: %s.', err)
+            self.exit_or_wait()
         except JSONDecodeError as err:
             logging.error('Failed to create a Rancher API client, please check the API connection details. JSONDecodeError: %s', err)
-            sys.exit()
+            self.exit_or_wait()
 
         return client
 
@@ -84,16 +88,16 @@ class RancherRegsitration:
         except rancher.ApiError as err:
             if re.search(r'Forbidden.*cannot create resource "clusters"', str(err)):
                 logging.error('Failed to register cluster %s, please check the API credentials as they do not allow the creation of clusters.', name)
-                sys.exit()
+                self.exit_or_wait()
             elif re.search(r'NotUnique.*Cluster name', str(err)):
                 logging.error('Failed to register cluster %s, a cluster with the same name already exists.', name)
-                sys.exit()
+                self.exit_or_wait()
             elif 'InvalidFormat' in str(err):
                 logging.error('Failed to register cluster, the format of the cluster name is invalid: %s.', name)
-                sys.exit()
+                self.exit_or_wait()
             else:
                 logging.error('Failed to register cluster %s, API error: %s.', name, err)
-                sys.exit()
+                self.exit_or_wait()
 
         # create registration token for the cluster
         token_created = False
@@ -107,9 +111,12 @@ class RancherRegsitration:
                     logging.warning('Unable to retrieve the cluster registration token for cluster: %s. Will retry in 1 second.', name)
                     count += 1
                     time.sleep(1)
+                else:
+                    logging.error('Unable to retrieve the cluster registration token for cluster: %s. ApiError: %s.', name, err)
+                    self.exit_or_wait()
         if count == 5:
             logging.error('Failed to retrieve a cluster registration token for cluster: %s. Max retry attempts reached.', name)
-            sys.exit()
+            self.exit_or_wait()
 
         # fetch registration token for the cluster
         import_manifest_url = reg_data.data_dict()['manifestUrl']
@@ -128,40 +135,54 @@ class RancherRegsitration:
         except urllib.error.HTTPError as err:
             logging.error('Failed to retrieve the import manifest (%s) for cluster: %s. Error: %s', import_manifest_url, name, err)
             self.unregister_cluster(name)
-            sys.exit()
+            self.exit_or_wait()
 
         # return name, url and location of path of import manifest
         return [name, import_manifest_url, str(storage_directory / manifest_file_name)]
 
     def unregister_cluster(self, name):
+        '''Unregister a cluster in a Rancher platform'''
         # get cluster by name
         try:
             cluster = self.client.list_cluster(name=name)
         except rancher.ApiError as err:
             logging.error('Failed to unregister cluster %s, API error', err)
-            sys.exit()
+            self.exit_or_wait()
 
         # get cluster by id
         try:
             cluster_id = cluster.data[0]['id']
         except IndexError:
             logging.error('Failed to unregister cluster, %s is not a registered cluster.', name)
-            sys.exit()
+            self.exit_or_wait()
 
         try:
             cluster = self.client.by_id_cluster(cluster_id)
         except rancher.ApiError as err:
             logging.error('Failed to unregister cluster %s, API error', err)
-            sys.exit()
+            self.exit_or_wait()
 
         # unregister cluster
         try:
             resp = self.client.delete(cluster)
         except rancher.ApiError as err:
             logging.error('Failed to unregister cluster %s, API error', err)
-            sys.exit()
+            self.exit_or_wait()
 
         return resp
+
+    def exit_or_wait(self):
+        '''Determine if the script exits or runs forever'''
+
+        if self.wait == 'True':
+            loop_forever = True
+            while loop_forever:
+                try:
+                    time.sleep(10)
+                except KeyboardInterrupt:
+                    sys.exit()
+        else:
+            sys.exit()
 
 def main():
 
@@ -172,10 +193,10 @@ def main():
     )
 
     # rancher connection details as positional arguments
-    parser.add_argument('--url', default=os.getenv('RANCHER_INTEGRATOR_URL'), required=True, help='Rancher url')
-    parser.add_argument('--username', default=os.getenv('RANCHER_INTEGRATOR_USERNAME'), required=True, help='API access key')
-    parser.add_argument('--password', default=os.getenv('RANCHER_INTEGRATOR_PASSWORD'), required=True, help='API secret key')
-    parser.add_argument('-i', '--insecure', default=os.getenv('RANCHER_INTEGRATOR_INSECURE'),  help='Toggle insecure https (True|False)')
+    parser.add_argument('-l', '--url', default=os.getenv('RANCHER_INTEGRATOR_URL'), help='Rancher url')
+    parser.add_argument('-u', '--username', default=os.getenv('RANCHER_INTEGRATOR_USERNAME'), help='API access key')
+    parser.add_argument('-p', '--password', default=os.getenv('RANCHER_INTEGRATOR_PASSWORD'), help='API secret key')
+    parser.add_argument('-c', '--cert_check', default=os.getenv('RANCHER_INTEGRATOR_CERT_CHECK'), help='Toggle certificate check https (True|False)')
     parser.add_argument('-w', '--wait', default=os.getenv('RANCHER_INTEGRATOR_WAIT'), help='Toggle run forever (True|False)')
 
     # subparser for the different commands e.g. register, unregister
@@ -187,7 +208,7 @@ def main():
 
     # unregister subparser
     unregister_parser = subparsers.add_parser('unregister',  help='UNREGISTER a cluster from rancher')
-    unregister_parser.add_argument('--name', default=os.getenv('RANCHER_INTEGRATOR_CLUSTER_NAME'), required=True, help='cluster name')
+    unregister_parser.add_argument('-n', '--name', default=os.getenv('RANCHER_INTEGRATOR_CLUSTER_NAME'), help='cluster name')
 
     # verify_api subparser
     verify_api_parser = subparsers.add_parser('verify', help='VERIFY the API credentials are suitable for cluster management')
@@ -195,16 +216,19 @@ def main():
     args = parser.parse_args()
 
     if args.command is not None:
+        if not args.url:
+            args.url = 'changeme'
         rancher_url = 'https://' + args.url + '/v3'
         access_key = args.username
         secret_key = args.password
+        wait = args.wait
 
         # create RancherRegsitration object with connection details
-        if args.insecure:
+        if args.cert_check == 'False':
             ssl._create_default_https_context = ssl._create_unverified_context
-            r = RancherRegsitration(rancher_url, access_key, secret_key, False)
+            r = RancherRegsitration(rancher_url, access_key, secret_key, wait, False)
         else:
-            r = RancherRegsitration(rancher_url, access_key, secret_key)
+            r = RancherRegsitration(rancher_url, access_key, secret_key, wait)
 
         if args.command == 'register':
             manifest = r.register_cluster(args.name)
@@ -215,14 +239,7 @@ def main():
         elif args.command == 'verify':
             resp = r.verify_api_client()
             print(resp)
-
-        if args.wait == True:
-            loop_forever = True
-            while loop_forever:
-                try:
-                    time.sleep(10)
-                except KeyboardInterrupt:
-                    loop_forever = False
+        r.exit_or_wait()
 
     else:
         parser.print_help()
